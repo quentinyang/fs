f7a3389210b71874bcbaf1d598aac548d05b2710
杨文锦 <quentin.yang@angejia.com>
2016-06-27 16:23:35 +0800

    refine home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '前端服务（FS）' });
 });
 
 module.exports = router;
